de116ecbe7916a47549911a1843d25974a645734
Danny Seara <dsseara@gmail.com>
2020-03-23 10:52:41 -0400

    changed make_eprPlot to work with Michaud data

--- freqent/oocyte/make_eprPlot.py
+++ freqent/oocyte/make_eprPlot.py
@@ -22,7 +22,7 @@ mpl.rcParams['xtick.direction'] = 'in'
 mpl.rcParams['ytick.direction'] = 'in'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--datapath', '-d', type=str,
+parser.add_argument('datapath', type=str,
                     help='parent directory of oocyte hdf5 files')
 parser.add_argument('--savepath', '-s', type=str, default=None,
                     help='path to output hdf5 files')
@@ -40,6 +40,9 @@ files = glob(os.path.join(datapath, '*.hdf5'))
 epr = []
 excitability = []
 f = []
+notes_actin = []
+notes_rho = []
+length = []
 
 for file in files:
     if file == '/mnt/llmStorage203/Danny/oocyte/160915_09.hdf5':
@@ -48,12 +51,15 @@ for file in files:
         if '/entropy' in d:
             f.append(file.split(os.path.sep)[-1].split('.')[0])
             epr.append(d['entropy']['s'][()])
+            length.append(d['images']['actin'][:].shape[0])
+            notes_actin.append(d['images']['actin'].attrs['Notes'])
+            notes_rho.append(d['images']['rho'].attrs['Notes'])
 
             # first two conditions work for bement data. last condition works with michaud data
             if d['images']['actin'].attrs['excitability'] == 'ctrl':
-                excitability.append(0)
+                excitability.append(-1)
             elif d['images']['actin'].attrs['excitability'] == 'Ect2':
-                excitability.append(1)
+                excitability.append(0)
             else:
                 excitability.append(d['images']['actin'].attrs['excitability'])
 
@@ -62,15 +68,17 @@ for file in files:
         else:
             pass
 
-epr_df = pd.DataFrame({'epr': epr, 'excitability': excitability, 'file': f})
-
+epr_df = pd.DataFrame({'epr': epr, 'excitability': excitability, 'file': f,
+                       'length': length, 'notes_actin': notes_actin})
+inds = np.logical_and(['ok' in note for note in epr_df['note_actin']],
+                      ['ok' in note for note in epr_df['note_rho']],
+                      epr_df['len'] >= 50)
 fig, ax = plt.subplots(figsize=(5, 5))
-
-sns.swarmplot(x='excitability', y='epr', data=epr_df, color='k', size=10, ax=ax)
-# ax.set(xlabel='excitability', ylabel=r'$ds/dt$', xticklabels=['ctrl', 'Ect2'])
+ax.plot(epr_df[inds]['excitability'], epr_df[inds]['epr'], 'ko')
+ax.set(xlabel=r'[ArhGAP] (ng/$\mu$l', ylabel=r'$ds/dt$')
 # sns.despine(offset={'left': -30}, trim=True)
 plt.tight_layout()
 
 if args.savepath is not None:
-    fig.savefig(os.path.join(savepath, today + '_epr_vs_ect2.pdf'.format(window=window, *sigma)))
+    fig.savefig(os.path.join(savepath, today + '_epr_vs_arhgap.pdf'.format(window=window, *sigma)))
 plt.show()
